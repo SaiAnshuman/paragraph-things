3900d402f3713644537beeecd085f0fac76387fe
Sai Playz <71771487+SaiAnshuman@users.noreply.github.com>
2021-01-07 16:04:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,13 +2,12 @@ function getParagraph1(){
 
  var inputs = [];
  
- for(var i = 1 ; i <=6 ; i++){
+ for(var i = 1 ; i <=4 ; i++){
 
    inputs.push(document.getElementById("para_input_box_" + i).value);
 
  }
 
- inputs.join(". ");
 
  document.getElementById("showParagraph1").innerHTML = inputs.join(". ");
 
@@ -20,13 +19,12 @@ function getParagraph2(){
 
   var inputs = [];
   
-  for(var i = 1 ; i <=6 ; i++){
+  for(var i = 1 ; i <=4 ; i++){
  
-    inputs.push(document.getElementById("para_input_box_" + i).value);
+    inputs.push(document.getElementById("para2_input_box_" + i).value);
  
   }
  
-  inputs.join(". ");
  
   document.getElementById("showParagraph2").innerHTML = inputs.join(". ");
  
